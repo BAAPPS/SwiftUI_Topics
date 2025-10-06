80dc7a70e6909643ae52b651716a46d1a662f592
BAAPPS <208905508+BAAPPS@users.noreply.github.com>
2025-10-05 19:25:55 -0700

    chore: initial commit for GeminiAPI_Model

---
+++ Data_Model/RemoteAPI/GeminiAPI_Model/GeminiAPI_Model/GeminiAPI_ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAPI_ModelApp.swift
+//  GeminiAPI_Model
+//
+//  Created by D F on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAPI_ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
